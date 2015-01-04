e0bea160e23722cea78a11d1cbc8ab7146bd664b
Barbie <barbie@missbarbell.co.uk>
2015-01-04 02:06:44 +0000

    Import of BARBIE/Template-Plugin-Lingua-EN-Numbers-0.02.tar.gz from CPAN.
    gitpan-cpan-distribution: Template-Plugin-Lingua-EN-Numbers
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         BARBIE/Template-Plugin-Lingua-EN-Numbers-0.02.tar.gz
    gitpan-cpan-author:       BARBIE
    gitpan-cpan-maturity:     released

--- lib/Template/Plugin/Lingua/EN/Numbers.pm
+++ lib/Template/Plugin/Lingua/EN/Numbers.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = 0.01;
+$VERSION = 0.02;
 
 use base qw(Template::Plugin);
 use Template::Plugin;
@@ -126,7 +126,7 @@ over from Andrew Ford.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright (C) 2014      Barbie for Miss Barbell Productions.
+Copyright (C) 2014-2015 Barbie for Miss Barbell Productions.
 
 This distribution is free software; you can redistribute it and/or
 modify it under the Artistic Licence v2.
